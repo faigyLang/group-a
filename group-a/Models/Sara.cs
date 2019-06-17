e00d34099b8fd88d439942b7c0b2f343cd39f97b
מחפודה שרה <208067652>
2019-06-17 10:05:49 +0300

    add new model sara

---
+++ group-a/Models/Sara.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace group_a.Models
+{
+    public class Sara
+    {
+        public string Id { get; set; }
+        //hello for evrybody bye..........☺☻♥☺☻♥☺☺☺
+    }
+}
